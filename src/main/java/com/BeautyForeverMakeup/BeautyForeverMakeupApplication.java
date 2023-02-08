965524b18d638a859fa80132f7feffce7c9e04af
mon0801 <123025761+mon0801@users.noreply.github.com>
2023-02-08 15:06:47 -0600

    EA-08-Practica-2 Monserrat Garro

---
+++ src/main/java/com/BeautyForeverMakeup/BeautyForeverMakeupApplication.java
@@ -0,0 +1,13 @@
+package com.BeautyForeverMakeup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeautyForeverMakeupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeautyForeverMakeupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/BeautyForeverMakeup/BeautyForeverMakeupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BeautyForeverMakeup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeautyForeverMakeupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
